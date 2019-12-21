75bfa4d60a2a63a0bf427b2df0347f03fef3281b
ashaftel <alexthebassoonguy@gmail.com>
2019-12-21 15:50:11 -0500

    added age of jim

--- hello.py
+++ hello.py
@@ -2,4 +2,6 @@
 import myname
 
 name = myname.get_name()
+age = myname.get_age()
 print("hello {}".format(name))
+print("He is {} years old".format(age))
--- myname.py
+++ myname.py
@@ -2,5 +2,8 @@
 def get_name():
     return "Jim"
 
+def get_age():
+    return "42"
+
 if __name__ == '__main__':
     print("testing: {}".format(get_name()))
